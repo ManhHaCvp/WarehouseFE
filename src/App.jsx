58e98dde01386467d71280e04fbb83546f9f06da
Ha Gia Manh <manhhacvp@gmail.com>
2025-01-22 19:42:10 +0700

    Test

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import React, { useState, useEffect, createContext } from "react";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import "react-day-picker/dist/style.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./App.scss";
+//instance
+import instance from "./utils/index";
+
+import HomePage from "./pages/main/HomePage";
+import LoginPage from "./pages/auth/LoginPage";
+import RegisterPage from "./pages/auth/RegisterPage";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import ForgotPasswordPage from "./pages/auth/ForgotPasswordPage";
+
+import NotFoundPage from "./pages/error/NotFoundPage";
+import ComingSoon from "./pages/error/ComingSoon";
+
+export const UserContext = createContext({});
+
+function App() {
+  const location = useLocation();
+  const [userAuth, setUserAuth] = useState({});
+  const isAdminRoute = location.pathname.includes("/admin");
+  const isUserRoute = location.pathname.includes("/user");
+
+  useEffect(() => {
+    const user = sessionStorage.getItem("user");
+    const accessToken = sessionStorage.getItem("accessToken");
+    if (user && accessToken) {
+      setUserAuth({ accessToken, user: JSON.parse(user) });
+    } else {
+      setUserAuth({ accessToken: null });
+    }
+  }, []);
+
+  return (
+    <>
+      <UserContext.Provider value={{ userAuth, setUserAuth }}>
+        <Toaster />
+        {!isAdminRoute && <Header />}
+        <main>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/home" element={<Navigate to="/" />} />
+
+            <Route path="/login" element={!userAuth.accessToken ? <LoginPage /> : <Navigate to="/" />} />
+            <Route path="/register" element={!userAuth.accessToken ? <RegisterPage /> : <Navigate to="/" />} />
+            <Route path="/forgot-password" element={!userAuth.accessToken ? <ForgotPasswordPage /> : <Navigate to="/" />} />
+
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </main>
+        {!isAdminRoute && !isUserRoute && <Footer />}
+      </UserContext.Provider>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/auth/LoginPage.jsx
@@ -0,0 +1,93 @@
+import React, { useState, useContext } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-hot-toast";
+import { UserContext } from "../../App"; // Import UserContext
+
+const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [rememberMe, setRememberMe] = useState(false); // Add state for Remember Me
+  const navigate = useNavigate();
+  const { setUserAuth } = useContext(UserContext); // Use setUserAuth from context
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:8080/api/auth/signin", {
+        username: email,
+        password,
+      });
+
+      if (response.status === 200) {
+        toast.success("Login successful");
+        const { token, username } = response.data;
+
+        if (rememberMe) {
+          localStorage.setItem("accessToken", token);
+          localStorage.setItem("username", JSON.stringify(username));
+        } else {
+          sessionStorage.setItem("accessToken", token);
+          sessionStorage.setItem("username", JSON.stringify(username));
+        }
+
+        setUserAuth({ token, username });
+        navigate("/");
+      }
+    } catch (error) {
+      toast.error(error.response?.data?.message || "Login failed");
+    }
+  };
+
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center pb-44">
+      <div className="w-full max-w-4xl p-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 bg-white p-6 rounded-lg shadow-lg">
+          <div>
+            <div className="flex justify-center">
+              <h2 className="text-3xl font-bold mb-4">Login</h2>
+            </div>
+            <form onSubmit={handleSubmit}>
+              <div className="mb-4">
+                <label className="flex justify-center text-gray-700 mb-5" htmlFor="email">
+                  Enter your email & password to log in
+                </label>
+                <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} className="w-full px-3 py-2 border rounded" placeholder="email@domain.com" />
+              </div>
+              <div className="mb-4">
+                <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} className="w-full px-3 py-2 border rounded" placeholder="Password" />
+              </div>
+              <div className="mb-4 flex items-center justify-between">
+                <label className="flex items-center">
+                  <input
+                    type="checkbox"
+                    className="form-checkbox"
+                    checked={rememberMe}
+                    onChange={(e) => setRememberMe(e.target.checked)} // Update state
+                  />
+                  <span className="ml-2">Remember Me</span>
+                </label>
+                <Link to={"/forgot-password"} className="text-sm text-blue-500 hover:underline">
+                  Forgot Password?
+                </Link>
+              </div>
+              <button type="submit" className="w-full bg-black text-white py-2 px-4 rounded hover:bg-gray-800">
+                Login
+              </button>
+            </form>
+            <div className="flex items-center my-4">
+              <hr className="flex-grow border-t border-gray-300" />
+              <span className="mx-2 text-gray-600">or continue with</span>
+              <hr className="flex-grow border-t border-gray-300" />
+            </div>
+          </div>
+          <div>
+            <img src="https://natrumax.com/wp-content/uploads/2021/08/logo-co-ban-NTM.jpg" alt="Advertisement" className="w-full h-auto rounded-lg" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
