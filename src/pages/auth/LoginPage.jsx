2bffa9fe45addb188fcf15ad4f986ae47f1c4c39
Ha Gia Manh <manhhacvp@gmail.com>
2025-03-04 17:47:36 +0700

    FE: Update login page

--- src/pages/auth/LoginPage.jsx
+++ src/pages/auth/LoginPage.jsx
@@ -2,18 +2,17 @@ import React, { useState, useContext } from "react";
 import axios from "axios";
 import { toast } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
-import { UserContext } from "../../App"; // Import UserContext
+import { UserContext } from "../../App";
 import { InputOTP, InputOTPGroup, InputOTPSlot } from "@/components/ui/input-otp";
 import { REGEXP_ONLY_DIGITS_AND_CHARS } from "input-otp";
 
 const LoginPage = () => {
   const [phone, setPhone] = useState("");
   const [otp, setOtp] = useState("");
-  const [showOtpForm, setShowOtpForm] = useState(false); // Điều khiển hiển thị OTP form
+  const [showOtpForm, setShowOtpForm] = useState(false);
   const navigate = useNavigate();
   const { setUserAuth } = useContext(UserContext);
 
-  // Hàm gửi OTP
   const handleSendOtp = async (e) => {
     e.preventDefault();
     if (!phone.match(/^(0[1-9]|84[1-9])[0-9]{8,9}$/)) {
@@ -23,28 +22,21 @@ const LoginPage = () => {
     try {
       await axios.post("http://localhost:8080/api/auth/request-otp", { phonenumber: phone }, { withCredentials: true });
       toast.success("OTP đã được gửi!");
-      setShowOtpForm(true); // Chuyển sang giao diện nhập OTP
+      setShowOtpForm(true);
     } catch (error) {
       console.error("Error sending OTP:", error);
       toast.error("Không thể gửi OTP, thử lại sau!");
     }
   };
 
-  // Hàm xác thực OTP
   const handleVerifyOtp = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post(
-        `http://localhost:8080/api/auth/verify-otp?phoneNumber=${phone}&otp=${otp}`,
-        {}, // Body rỗng
-        { withCredentials: true },
-      );
-
+      const response = await axios.post(`http://localhost:8080/api/auth/verify-otp?phoneNumber=${phone}&otp=${otp}`, {}, { withCredentials: true });
       toast.success("Xác thực thành công!");
       sessionStorage.setItem("user", JSON.stringify(response.data));
-      setUserAuth(response.data); // Cập nhật trạng thái đăng nhập
-      console.log(response.data);
-      navigate("/admin/dashboard"); // Điều hướng tới trang chính
+      setUserAuth(response.data);
+      navigate("/admin/dashboard");
     } catch (error) {
       console.error("OTP verification failed:", error);
       toast.error("OTP không hợp lệ, vui lòng thử lại!");
@@ -52,60 +44,40 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 h-screen">
-      {/* Cột bên trái */}
-      <div 
-  className="flex items-center justify-center w-full h-screen bg-cover bg-center" 
-  style={{ backgroundImage: "url('https://natrumax.com/wp-content/uploads/2021/08/logo-co-ban-NTM.jpg')" }}
->
- 
-</div>
+    <div className="flex h-screen w-screen overflow-hidden">
+      <div className="flex flex-col justify-center items-center w-1/2 h-full bg-[#182F73] rounded-3xl">
+        <div className="absolute top-4 left-4 flex items-center px-4 py-2 rounded-md">
+          <img src="/icon.svg" alt="Logo" className="w-10 h-10 mr-2" />
+          <span className="text-lg font-bold text-white">NATRUMAX</span>
+        </div>
+        <img src="/icon.svg" alt="Logo" className="w-80 h-80" />
 
-      {/* Cột bên phải: Chuyển đổi giữa 2 form */}
-      <div className="flex items-center justify-center">
-        <div className="flex flex-col justify-center w-94">
+        <div className="absolute bottom-4 left-4 text-white text-sm leading-tight ml-5 mb-8">
+          <p className="text-xl"> “NATRUMAX - VÌ MỘT VIỆT NAM KHỎE MẠNH”</p>
+          <p className="mt-2">Nguyễn Tất Tùng</p>
+        </div>
+      </div>
+
+      <div className="flex flex-col justify-center items-center w-1/2 h-full">
+        <div className="w-94">
           {!showOtpForm ? (
-            // Form nhập số điện thoại
-            <form onSubmit={handleSendOtp} className=" flex flex-col justify-center w-94  ">
-              <div className="flex justify-center">
-                <h2 className="text-3xl font-semibold mb-4">Login</h2>
-              </div>
-              <div className="mb-4">
-                <label className="flex justify-center text-gray-700 mb-5" htmlFor="phone">
-                  Enter phone number to get OTP
-                </label>
-                <div className="flex justify-center">
-                  <input type="text" id="phone" value={phone} onChange={(e) => setPhone(e.target.value)} className="w-full px-3 py-2 border rounded" placeholder="Phone Number" />
-                </div>
-              </div>
-              <button type="submit" className=" w-full bg-[#2a4181] text-white py-2 px-4 rounded hover:bg-[#1E40AF]">
+            <form onSubmit={handleSendOtp} className="flex flex-col justify-center w-full">
+              <h2 className="text-3xl font-semibold text-center mb-4">Login</h2>
+              <label className="text-gray-700 text-center mb-5" htmlFor="phone">
+                Enter phone number to get OTP
+              </label>
+              <input type="text" id="phone" value={phone} onChange={(e) => setPhone(e.target.value)} className="w-full px-3 py-2 border rounded" placeholder="Phone Number" />
+              <button type="submit" className="w-full bg-[#2a4181] text-white py-2 px-4 rounded hover:bg-[#1E40AF] mt-1.5">
                 Get OTP
               </button>
             </form>
           ) : (
-            // Form nhập OTP
-            <form onSubmit={handleVerifyOtp}>
-              <div className="flex justify-center">
-                <h2 className="text-3xl font-semibold mb-4">Submit OTP</h2>
-              </div>
-              <div className="mb-4">
-                <label className="flex justify-center text-gray-700 mb-5" htmlFor="otp">
-                  Enter OTP to access into system
-                </label>
-                {/* <input
-                  type="text"
-                  id="otp"
-                  value={otp}
-                  onChange={(e) => setOtp(e.target.value)}
-                  className="w-full px-3 py-2 border rounded"
-                  placeholder="Enter OTP"
-                /> */}
-              </div>
-              <div className="flex justify-center w-full ">
-              <InputOTP maxLength={6} pattern={REGEXP_ONLY_DIGITS_AND_CHARS}
-                 value={otp} // Set giá trị OTP từ state
-                 onChange={setOtp} // Cập nhật state khi người dùng nhập
-              >
+            <form onSubmit={handleVerifyOtp} className="flex flex-col justify-center w-full">
+              <h2 className="text-3xl font-semibold text-center mb-4">Submit OTP</h2>
+              <label className="text-gray-700 text-center mb-5" htmlFor="otp">
+                Enter OTP to access into system
+              </label>
+              <InputOTP maxLength={6} pattern={REGEXP_ONLY_DIGITS_AND_CHARS} value={otp} onChange={setOtp} className="flex justify-center">
                 <InputOTPGroup>
                   <InputOTPSlot index={0} />
                   <InputOTPSlot index={1} />
@@ -115,20 +87,15 @@ const LoginPage = () => {
                   <InputOTPSlot index={5} />
                 </InputOTPGroup>
               </InputOTP>
-              </div>
-              
-              <button type="submit" className="w-full bg-[#2a4181] text-white py-2 px-4 rounded hover:bg-[#1E40AF] mt-3">
+              <button type="submit" className="w-full bg-[#2a4181] text-white py-2 px-4 rounded hover:bg-[#1E40AF] mt-4">
                 Submit
               </button>
             </form>
           )}
-
-          <div className="flex items-center my-4">
-            <span className="mx-2 text-gray-600">
-              By clicking get OTP, you agree to our <br />
-              <span className="underline ">Terms of Service</span> and <span className="underline ">Privacy Policy</span>
-            </span>
-          </div>
+          <p className="text-center text-gray-600 mt-4">
+            By clicking get OTP, you agree to our <br />
+            <span className="underline">Terms of Service</span> and <span className="underline">Privacy Policy</span>
+          </p>
         </div>
       </div>
     </div>
